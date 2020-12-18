fa7692ffde8aca8ea87a7a52f9538111e37248cf
Lorena Coelho <lorenagfcoelho@gmail.com>
2020-12-17 23:56:37 -0300

    Validade com erro

--- app/models/study_item.rb
+++ app/models/study_item.rb
@@ -1,3 +1,9 @@
 class StudyItem < ApplicationRecord
     validates :title, :category, presence: {message: 'não pode ficar em branco!'}
+
+    def overdue?
+        if due_date > Date.current && status == 'Concluído'
+            return false
+        end
+    end
 end
